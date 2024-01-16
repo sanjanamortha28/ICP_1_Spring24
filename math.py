df40e9a68fef9dca4323ac4cb97df8d675c5b3f0
Sanjana Mortha <70304377+sanjanamortha28@users.noreply.github.com>
2024-01-15 20:21:36 -0600

    Update math.py

--- math.py
+++ math.py
@@ -1,13 +1,13 @@
-def mth(x,y):
+def mth(x,y): #function defining
  print(x + y)
- print(x - y)
+ print(x - y) #performing the arthemetic operrations
  print(x / y)
  print(x * y)
 
-x=int(input("enter the 1st number:"))
+x=int(input("enter the 1st number:")) #taking the input from user
 y=int(input("enter the 2nd number:"))
 
-mth(x,y)
+mth(x,y) #calling the function to perform
 
  
 
