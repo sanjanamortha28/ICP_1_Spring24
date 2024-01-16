f1689d8665aaede79d1c6c3a1949ade7c6484729
Sanjana Mortha <70304377+sanjanamortha28@users.noreply.github.com>
2024-01-15 23:14:33 -0600

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,3 +1,5 @@
 txt = str(input("enter the string:")) #taking the string input
-x = txt.replace("python", "pythons")  #using replace keyword
+x = txt.replace("python", "pythons")#using replace keyword
+x = txt.replace("Python", "Pythons")
+x = txt.replace("PYTHON", "PYTHONS")
 print(x) #printiung the result
