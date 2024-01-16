197834bad53057b841062a57d19b690ef8bc97c3
Sanjana Mortha <70304377+sanjanamortha28@users.noreply.github.com>
2024-01-15 20:23:17 -0600

    Update string rev.py

--- string rev.py
+++ string rev.py
@@ -1,12 +1,12 @@
-def removechar(string, x, y):
+def removechar(string, x, y): #defining the function
     first = string.replace(x,'')
     result = first.replace(y,'')
-    return result
+    return result #returning the resultant string
 string = input("Enter a string: ")
-x= input("Enter the character to remove: ")
+x= input("Enter the character to remove: ") #taking the inputs from user
 y= input("Enter the character to remove: ")
 
-result = removechar(string, x, y)
+result = removechar(string, x, y) #calling the function
 print(f"String after removing '{x}','{y}' : {result}")
 print(result[::-1])
 
